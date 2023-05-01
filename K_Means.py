2456e4d3b4f567192a1a2c5b496b5afc4edfa763
Modaser <Modasergomis@gmail.com>
2023-05-01 17:22:18 +0200

    h

--- K_Means.py
+++ K_Means.py
@@ -10,11 +10,10 @@ Values = Values[:,1:]
 
 numOfCentroid = int(input('Please enter the number of cluster '))
 
-distanceType = input('Please type 1 for Euclidean distance and any another number for Manhattan distance ')
-
 out = int(input('Please enter the outlier threshold '))
 
-def k_means_clustering(Values, numOfCentroid, distanceType):
+'''this fun  takes a dataset values and number of centroid and the way of calculate distance  '''
+def k_means_clustering(Values, numOfCentroid, ):
     centroids = data.sample(n=numOfCentroid).values
     centroids = centroids[:,1:]
     for i in range(len(centroids)):
@@ -24,46 +23,42 @@ def k_means_clustering(Values, numOfCentroid, distanceType):
     clusters = {}
     d=0
     while notgood:
+        
         print(f'iteration {d+1}')
         for index, data1 in enumerate(Values):
             list_of_distances = []
             for index1, centroid in enumerate(centroids):
-                if distanceType == 1:
-                    distance1=0
-                    for i in range(len(data1)):
-                        distance1+=(centroid[i]-data1[i])**2
-                    distance=np.sqrt(distance1) 
-                else:
-                    distance2=0
-                    for i in range(len(data1)):
-                        distance2+=abs(centroid[i]-data1[i])
-                    distance=abs(distance2) 
+                distance2=0
+                for i in range(len(data1)):
+                    distance2+=abs(centroid[i]-data1[i])
+                distance=abs(distance2) 
                 list_of_distances.append(distance)
-
-            clusters[index] = list_of_distances.index(min(list_of_distances))        
-        new_centroids = pd.DataFrame(Values).groupby(by=clusters).mean().values
-        x=new_centroids
+            clusters[index] = list_of_distances.index(min(list_of_distances))   
+        newCentroids = pd.DataFrame(Values).groupby(by=clusters).mean().values
+        x=newCentroids
         for i in range(len(x)):
              print(f'New Centroid_{i+1}:{x[i]}')
         print('-'*100)
-        if np.count_nonzero((centroids)-(new_centroids)) == 0:
+        if np.count_nonzero((centroids)-newCentroids) == 0:
             notgood = False
         else:
-            centroids = new_centroids
+            centroids = newCentroids
         d+=1
     return clusters, centroids
-clusters, centroids = k_means_clustering(Values, numOfCentroid, distanceType)
+clusters, centroids = k_means_clustering(Values, numOfCentroid)
 distance4 = {}
 g=0
-for index, rows in enumerate(Values):
-    print(f'[{firstval[g]}: {rows}]--> centroid {clusters[index]+1}')
-    desired_cluster = clusters[index]
+print(Values[0])
+for i in range (len(Values)):
+    print(f'[{firstval[g]}: {Values[i]}]--> centroid {clusters[i]+1}')
+    tempcluster = clusters[i]
     distance3=0
-    for i in range(len(rows)):
-        distance3+=(centroids[desired_cluster][i]-rows[i])**2
-    dist = np.sqrt(distance3)
-    distance4[index, dist] = desired_cluster
+    for j in range(len(Values[i])):
+        distance3+=abs(centroids[tempcluster][j]-Values[i][j])
+    dist =distance3
+    distance4[i, dist] =tempcluster
     g+=1
+    
 print('-'*100)
 
 sort_by_value = dict(sorted(distance4.items(), key=lambda item: item[1]))
@@ -77,15 +72,22 @@ d=new_dictionary.items()
 for k, val in d :
     print(f'cluter {k+1} has a {len(val)} items')
     sortedItems = sorted(val, key = lambda x: x[1])
-    print(f'cluster with distances {k+1} is')
+    print(f'cluster {k+1} with distances is')
     for i in range(len(sortedItems)):
         print(f'[{firstval[sortedItems[i][0]]}-->{sortedItems[i][1]}]--> cluster {k+1}')
     for i in range(len(sortedItems)):
-        if(sortedItems[i][1]>out):
+        if(sortedItems[i][1]>float(out)):
             xx=i
             break
     print('-'*100)
     print(f'outlier of cluster {k+1} :')
-    for i in range (xx,len(sortedItems)):
-        print(f'outlier of cluter {k+1} -->{firstval[sortedItems[i][0]]}')
-    print('-'*100)
+    if(sortedItems[0][0]>float(out)):
+       for i in range (0,len(sortedItems)):
+            print(f'outlier of cluter {k+1} -->{firstval[sortedItems[i][0]]}-->{sortedItems[i][1]}')
+    else:
+        if (xx==0):
+            print('There is no outlier')
+        else:
+            for i in range (xx,len(sortedItems)):
+                print(f'outlier of cluter {k+1} -->{firstval[sortedItems[i][0]]}-->{sortedItems[i][1]}')
+        print('-'*100)
